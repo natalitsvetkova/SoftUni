95e22d2299301c2aae9de29eef3542ab0b5a82de
natalitsvetkova <155837764+natalitsvetkova@users.noreply.github.com>
2024-01-07 18:43:30 +0200

    Delete 09. Yard Greening.py

--- 09. Yard Greening.py
+++
@@ -1,6 +0,0 @@
-square_meters_for_greening = float(input())
-price = square_meters_for_greening * 7.61
-discount = price * 0.18
-final_sum = price - discount
-print(f"The final price is: {final_sum} lv.")
-print(f"The discount is: {discount} lv.")
